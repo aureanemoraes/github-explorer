a3fabf74b507b363d8b24f8f408c6de7d9b7ee70
aureane.moraes <aureane.moraes@gmail.com>
2021-04-16 16:01:21 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+// Components
+import {RepositoryList} from './components/RepositoryList';
+// Styles
 import './styles/global.scss';
 
 export function App() {
     //throw new Error('Erro de teste!');
-    return <h1>Hello, World!</h1>
+    return <RepositoryList />;
+
 }
